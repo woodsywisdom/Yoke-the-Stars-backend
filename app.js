30251c44a5e483c872bf25cb1c835f6cf2dc62c8
Will MacCarty <wmaccarty@gmail.com>
2021-04-13 00:04:29 -0400

    created mongodb with admin user 'yoker' and connected in app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( () => console.log("Mongoose db up"))
+    .catch( err => console.log(err));
+
+
+
+
+
+
 app.get('/', (req, res) => res.send('Working'));
 
 const port = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://yoker:1ePmomO9cNRfeJLN@cluster0.dbzlx.mongodb.net/test'
+};
+
+const username = 'yoker';
+const pw = '1ePmomO9cNRfeJLN';
