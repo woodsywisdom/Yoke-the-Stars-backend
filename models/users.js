db1c2383d8c5fe3cd2d97c46aec1b9b71cc0d1a3
Will MacCarty <wmaccarty@gmail.com>
2021-04-13 23:50:09 -0400

    Added password hashing and validation to users model

--- models/users.js
+++ models/users.js
@@ -1,18 +1,46 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secret = require('../confix')
 
 const UserSchema = mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
+        match: [ /^[a-zA-Z0-9]+$/, 'username contains invalid characters'],
         index: true
     },
     email: {
         type: String,
         required: true,
+        unique: true,
+        match: [ /\S+@\S+\.\S+/, 'not a valid email' ],
         index: true
     },
+    hash: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    }
 }, { timestamps: true });
+
+UserSchema.plugin(uniqueValidator, {message: 'This username or email is already being used'});
+
+UserSchema.methods.setPassword = function(pw) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(pw, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function(pw) {
+    const hash = crypto.pbkdf2Sync(pw, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
 
 mongoose.model('User', UserSchema);
 
